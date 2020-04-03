d6a0051824579be94c3454e63a4645fd8755683b
Oleg Voloshyn <sparco2405@gmail.com>
2020-04-03 21:41:58 +0300

    Add actions for controller articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,27 @@
 class ArticlesController < ApplicationController
-  def new    
+
+  def new 
+  end  
+  
+
+  def create 
+  @article = Article.new(article_params)
+
+  @article.save
+  redirect_to @article   # В чем разница????
+  #render action: 'show'
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index   
+  @articles = Article.all
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
